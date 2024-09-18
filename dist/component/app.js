7210ecd93d64b1e0b5dd9340b04d922c202889a9
Yankhoba <bagayoko.yan@gmail.com>
2024-09-18 10:10:28 +0200

    last push

--- dist/component/app.js
+++ dist/component/app.js
@@ -1,11 +1,17 @@
 class App {
     appElement = document.querySelector(".app");
-    init = () => { };
+    init = async () => {
+        await this.display();
+    };
     getShow = async () => {
-        const request = new Request(": https://api.reelgood.com/v3.0/content/random?sources=netflix");
+        const request = new Request("https://api.reelgood.com/v3.0/content/random?sources=netflix");
         const response = await fetch(request);
         const data = await response.json();
         return data;
+    };
+    display = async () => {
+        const data = await this.getShow();
+        console.log(data);
     };
 }
 export default App;
--- src/component/app.ts
+++ src/component/app.ts
@@ -6,14 +6,21 @@ class App {
     ".app"
   ) as HTMLDivElement;
 
-  public init = (): void => {};
+  public init = async (): Promise<void> => {
+    await this.display();
+  };
   private getShow = async (): Promise<Show | TvShow> => {
     const request: Request = new Request(
-      ": https://api.reelgood.com/v3.0/content/random?sources=netflix"
+      "https://api.reelgood.com/v3.0/content/random?sources=netflix"
     );
     const response = await fetch(request);
-    const data:Show | TvShow = await response.json();
+    const data: Show | TvShow = await response.json();
     return data;
+  };
+
+  private display = async (): Promise<void> => {
+    const data = await this.getShow();
+    console.log(data);
   };
 }
 export default App;
